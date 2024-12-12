963196a92a555770bf72ec80b9193c2c8262b71d
nidhibarevadiya <nidhi456barevadiy@gmail.com>
2024-12-12 10:42:50 +0530

    nidhi

---
+++ lec4/loop3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i;
+    cin>>i;
+    for(i;i<=10;i++){
+        for(int j=10 ; j<=20 ;j++){
+            cout <<i << " " <<j<<endl;
+        }
+    }
+    return 0;
+}
