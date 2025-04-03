577867e1674f5a246861ec3ee3b01c589571f7cf
nidhibarevadiya <nidhi456barevadiy@gmail.com>
2025-04-03 10:24:34 +0530

    dsa

--- finalexam/q2.cpp
+++ finalexam/q2.cpp
@@ -16,11 +16,22 @@ void twoSumBru(int arr[], int n, int target) {
 }
 
 
-
+void twoSumOpt(int arr[], int n, int target) {
+    unordered_map<int, int> numMap;
+    for (int i = 0; i < n; i++) {
+        int complement = target - arr[i];
+        if (numMap.find(complement) != numMap.end()) {
+            cout << "Indices of the two numbers: " << numMap[complement] << ", " << i  << endl;
+            return;
+        }
+        numMap[arr[i]] = i;
+    }
+    cout << "No pair found using Optimized approach." << endl;
+}
 
 int main() {
     int n, target;
-    cout << "Enter the number  of the array: ";
+    cout << "Enter the number of elements in the array: ";
     cin >> n;
     
     int arr[n];
@@ -33,9 +44,13 @@ int main() {
     cin >> target;
     
     twoSumBru(arr, n, target);
-
+    twoSumOpt(arr, n, target);
     
     return 0;
 }
+
+
+
+
 
 
